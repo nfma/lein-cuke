df934beb19441234c36f954073c6b80de7a323b0
Nuno Marques <nuno@platogo.com>
2010-12-21 15:37:12 +0100

    merged

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject org.clojars.mjul/lein-cuke "0.3.3-PATCH"
+(defproject org.clojars.mjul/lein-cuke "1.1.0"
   :description "Cucumber runner for Leiningen (lein)"
-  :dependencies [[leiningen "1.3.1"] 
-                 [gherkin "2.2.4"]
-                 [cuke4duke "0.3.2" :exclusions [gherkin]]]
+  :repositories {"codehaus" "http://repository.codehaus.org",
+		 "cukes" "http://cukes.info/maven"}
+  :dependencies [[cuke4duke "0.4.2"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
-
